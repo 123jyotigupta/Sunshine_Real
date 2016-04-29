062318cf709e14a8b6c3d39e3c3c53dd3d97502b
Jyoti Gupta <jyotigupta275@gmail.com>
2016-04-29 14:45:15 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/sunshine/android/sunshine_real/ApplicationTest.java
@@ -0,0 +1,33 @@
+package com.sunshine.android.sunshine_real;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+//public class ApplicationTest extends ApplicationTestCase<Application> {
+//    public ApplicationTest() {
+//        super(Application.class);
+//    }
+//}
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+        import junit.framework.Test;
+        import junit.framework.TestSuite;
+
+public class ApplicationTest extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(ApplicationTest.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public ApplicationTest() {
+        super();
+    }
+}
---
+++ app/src/main/java/com/sunshine/android/sunshine_real/DetailFragment.java
@@ -0,0 +1,20 @@
+package com.sunshine.android.sunshine_real;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by developeri on 4/18/16.
+ */
+public class DetailFragment extends Fragment {
+
+   // @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View rootview  = inflater.inflate(R.layout.fragment_detail , container , false);
+//        return rootview;
+//    }
+}
